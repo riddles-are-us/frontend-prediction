b0fb26ae3635bf3333db3879e96c9f88510e26c0
JupiterXiaoxiaoYu <jupiterxiaoxiaoyu@gmail.com>
2025-06-30 19:27:59 +0800

    remove empty reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { DelphinusReactProvider, setProviderConfig } from 'zkwasm-minirollup-browser';
-import { store } from './app/store';
 import { Toaster as Sonner } from "./components/ui/sonner";
 import { Toaster } from "./components/ui/toaster";
 import { TooltipProvider } from "./components/ui/tooltip";
@@ -17,28 +15,26 @@ setProviderConfig({ type: 'rainbow' });
 const queryClient = new QueryClient();
 
 const App = () => (
-  <Provider store={store}>
-    <DelphinusReactProvider appName="ZKWASM-PREDICTION-MARKET">
-      <QueryClientProvider client={queryClient}>
-        <BrowserRouter>
-          <TooltipProvider>
-            <Toaster />
-            <Sonner />
-            <Routes>
-              <Route path="/" element={<MarketList />} />
-              <Route path="/:marketId" element={
-                <MarketProvider>
-                  <Index />
-                </MarketProvider>
-              } />
-              {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </TooltipProvider>
-        </BrowserRouter>
-      </QueryClientProvider>
-    </DelphinusReactProvider>
-  </Provider>
+  <DelphinusReactProvider appName="ZKWASM-PREDICTION-MARKET">
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <Routes>
+            <Route path="/" element={<MarketList />} />
+            <Route path="/:marketId" element={
+              <MarketProvider>
+                <Index />
+              </MarketProvider>
+            } />
+            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </TooltipProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
+  </DelphinusReactProvider>
 );
 
 export default App;
