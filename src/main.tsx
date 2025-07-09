bcfd0d62c4e5da26b6f2ae13d01bba3f44a900f8
JupiterXiaoxiaoYu <jupiterxiaoxiaoyu@gmail.com>
2025-07-09 21:56:15 +0800

    add log

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import { setProviderConfig, setRpcUrl } from 'zkwasm-minirollup-browser'
+import { setProviderConfig, setRpcUrl, getRpcUrl } from 'zkwasm-minirollup-browser'
 import App from './App'
 import './index.css'
 
 // Configure the provider before app initialization - must be called before DelphinusReactProvider
 setProviderConfig({ type: 'rainbow' });
 setRpcUrl();
+console.log("fullUrl is", getRpcUrl());
 
 createRoot(document.getElementById("root")!).render(<App />);
 
